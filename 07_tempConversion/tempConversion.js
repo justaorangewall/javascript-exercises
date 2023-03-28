8e175bc2767db7ba6ec76397ca2c8455b00ab740
JustAOrangeWall <128050806+justaorangewall@users.noreply.github.com>
2023-03-28 01:53:49 -0400

    solved exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faren) {
+  return Math.round((faren-32)*(5.0/9.0)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celci) {
+  return Math.round((celci*(9.0/5.0)+32)*10)/10;
 };
 
 // Do not edit below this line
