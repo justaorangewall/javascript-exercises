e0addecf2d1b4f7c847f331ba67929872ba1bea8
JustAOrangeWall <128050806+justaorangewall@users.noreply.github.com>
2023-03-28 01:29:58 -0400

    Solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(typeof start!=="number"||typeof end!=="number")return 'ERROR';//return ERROR if the values are not number.
+    
+    if(start<0||end<0)return 'ERROR';//return ERROR if the numbers are negative numbers.
+    let sum=0;
+    if(start<end){
+        for(;start<=end;start++)
+        sum+=start;
+    }
+    else if(start>end){
+        for(;start>=end;start--)
+        sum+=start;
+    }
+    return sum;
 };
 
 // Do not edit below this line
