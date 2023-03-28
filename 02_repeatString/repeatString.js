7cd05bf0726139d414a26d20f8c468ff534fb842
JustAOrangeWall <128050806+justaorangewall@users.noreply.github.com>
2023-03-28 00:10:49 -0400

    Solved exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    if(num<0) return `ERROR`
+    sumText="";
+    while(num>0)
+    {
+        sumText+=text;
+        num--;
+    }
+    return sumText;
 };
 
 // Do not edit below this line
