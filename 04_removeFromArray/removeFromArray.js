2cb9eae591c451c0c1e47476be0a83b6e4c792a2
JustAOrangeWall <128050806+justaorangewall@users.noreply.github.com>
2023-03-28 01:13:08 -0400

    Solved Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray,...removal) {
+    lengofOG=originalArray.length;
+    lengOfRemoval=removal.length;
+    newArray=[];
+    for(let index=0;index<lengofOG;index++){ //iterate through original array
+        let foundDupe=false;
+        for(let newArI=0;newArI<lengOfRemoval;newArI++){//compare each original element to element to be removed and flag if found.
+            
+            
+            if(originalArray[index]===removal[newArI])foundDupe=true;
+            
+        }
+        if(foundDupe==false)newArray.push(originalArray[index]);//only push into new array if it's not meant to be removed.
+    }
+    return newArray;
 };
 
 // Do not edit below this line
