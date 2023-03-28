933e843910f32fe8698d75180d5d27d2b0116e88
JustAOrangeWall <128050806+justaorangewall@users.noreply.github.com>
2023-03-28 00:27:05 -0400

    Solved exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    reversedText=""
+    charCount=-1;
+    while(reversedText.length<text.length){
+        reversedText+=text.substr(charCount,1);
+        charCount--;
+    }
+    return reversedText;
 };
 
 // Do not edit below this line
