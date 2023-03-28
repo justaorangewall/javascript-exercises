79dc5ef151c45ff86929a730dc3b2e5eea13776f
JustAOrangeWall <128050806+justaorangewall@users.noreply.github.com>
2023-03-28 01:42:31 -0400

    solved exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    leapYes=false;
+    return year%4===0 && (year%400===0||year%100!==0)
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
