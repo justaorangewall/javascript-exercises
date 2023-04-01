44fed31620a0c5ab803fb3b09ea830cca7d9d2c4
justaorangewall <128050806+justaorangewall@users.noreply.github.com>
2023-04-01 16:11:47 -0400

    setup for jest, solved 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a=0,b=0) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a=0,b=0) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  total=0;
+	return array.reduce((total,num)=> total=total+num,total=0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((total,num)=> total=total*num,total=1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	powerStep=0;
+  total=1;
+  while(powerStep<b){total=total*a; powerStep++;}
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num===0)return 1;
+  total=1;
+  for(let i=1;i<=num;i++)total=total*i;
+  return total;
 };
 
 // Do not edit below this line
