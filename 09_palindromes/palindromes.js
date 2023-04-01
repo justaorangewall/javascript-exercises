29176994ab0c54d2aa95046dadffafcc1eaabb98
justaorangewall <128050806+justaorangewall@users.noreply.github.com>
2023-04-01 19:13:29 -0400

    solved exercise 08

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let reverseOGword="";
+    word=word.toLowerCase();
 
+    let array=word.split(""); 
+    array=array.filter((char) => {
+        charUpper=char.toUpperCase();
+        if(charUpper.toLowerCase()!==charUpper)
+        {return 1;}
+    })
+    comparisonWord=array.join("");
+    index=0;
+    while(index<comparisonWord.length){
+        reverseOGword=`${comparisonWord.charAt(index)}${reverseOGword}`;
+        index++;
+    }
+
+    return reverseOGword===comparisonWord;
+    
 };
 
 // Do not edit below this line
