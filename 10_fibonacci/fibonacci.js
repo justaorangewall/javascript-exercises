5ee9cc805c6b7f6e7700d57a2e1d3d4e8be324bb
justaorangewall <128050806+justaorangewall@users.noreply.github.com>
2023-04-01 20:00:22 -0400

    solved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<0)return 'OOPS';
+    let fiboSum=0;
+    if(num==0)return 0;
+    if(num==1) return 1;
+    fiboSum=fibonacci(num-1)+fibonacci(num-2);
+    return fiboSum;
 };
 
 // Do not edit below this line
