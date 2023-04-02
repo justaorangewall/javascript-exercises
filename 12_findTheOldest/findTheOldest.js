1e0b37cc19afe70be48f2a194da96d7365a3008c
justaorangewall <128050806+justaorangewall@users.noreply.github.com>
2023-04-01 21:30:26 -0400

    solved exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+   return people.reduce((oldest,person)=>{
+        console.log(oldest);
+        current=getAge(person.yearOfBirth,person.yearOfDeath);//age of current element in the loop
+        oldAge=getAge(oldest.yearOfBirth,oldest.yearOfDeath);//age of the oldest so far.
+        
+        return oldAge<current ?  person: oldest;
+    }
+    );
+   
 };
 
+const getAge = function (birthyear, deathyear) {
+    if(deathyear=='undefined')death=new Date().getFullYear();
+    return deathyear-birthyear;
+};
 // Do not edit below this line
 module.exports = findTheOldest;
